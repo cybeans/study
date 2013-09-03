79c52685ac33f2ed5868983ea3f3dd804d079a7f
kkoba <kkoba@plowman.co.jp>
2013-09-03 16:19:50 +0900

    test

---
+++ Study/src/jp/co/cybeans/Study.java
@@ -0,0 +1,15 @@
+package jp.co.cybeans;
+
+public class Study {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("test");
+		System.out.println("yoshioka");
+		System.out.println("jojo");
+	}
+
+}
